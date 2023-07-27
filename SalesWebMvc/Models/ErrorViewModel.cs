dfa7e5bfb55f96a3e9dee850f79518ffed7621ca
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-27 08:49:09 -0300

    Entity Framework

--- SalesWebMvc/Models/ErrorViewModel.cs
+++ SalesWebMvc/Models/ErrorViewModel.cs
@@ -1,10 +1,11 @@
 using System;
 
-namespace SalesWebMvc.Models.ViewModels
+namespace SalesWebMvc.Models
 {
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
